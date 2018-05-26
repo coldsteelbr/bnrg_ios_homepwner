0c4b64e8fd87aa69400e44cc8d71f711cc593eb5
Roman <cold.steel.br@gmail.com>
2018-05-26 23:11:11 +0700

    Table header, Edit & Add buttons added

--- bnrg_ios_homepwner/ItemsViewController.swift
+++ bnrg_ios_homepwner/ItemsViewController.swift
@@ -11,6 +11,14 @@ import UIKit
 class ItemsViewController: UITableViewController{
     var itemStore: ItemStore!
     
+    @IBAction func addNewItem(_ sender: UIButton){
+        print("Add")
+    }
+    
+    
+    @IBAction func toggleEditingMode(_ sender: UIButton){
+        print("Edit")
+    }
     
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
