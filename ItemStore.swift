999df38c798c4964b5006ccb3fadc78594104eeb
Roman <cold.steel.br@gmail.com>
2018-05-27 12:37:01 +0700

    Deleted dublicate ItemStore.swift

--- ItemStore.swift
+++
@@ -1,9 +0,0 @@
-//
-//  ItemStore.swift
-//  bnrg_ios_homepwner
-//
-//  Created by Roman Brazhnikov on 26.05.2018.
-//  Copyright © 2018 Roman Brazhnikov. All rights reserved.
-//
-
-import Foundation
