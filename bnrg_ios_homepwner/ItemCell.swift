984153365f13370ac64f4e19bf1e9155d380d764
Roman <cold.steel.br@gmail.com>
2018-05-27 16:45:28 +0700

    Custom ItemCell added

---
+++ bnrg_ios_homepwner/ItemCell.swift
@@ -0,0 +1,14 @@
+//
+//  ItemCell.swift
+//  bnrg_ios_homepwner
+//
+//  Created by Roman Brazhnikov on 27.05.2018.
+//  Copyright © 2018 Roman Brazhnikov. All rights reserved.
+//
+
+import UIKit
+class ItemCell: UITableViewCell{
+    @IBOutlet var nameLabel: UILabel!
+    @IBOutlet var serialNumberLabel: UILabel!
+    @IBOutlet var valueLabel: UILabel!
+}
--- bnrg_ios_homepwner/ItemsViewController.swift
+++ bnrg_ios_homepwner/ItemsViewController.swift
@@ -33,6 +33,12 @@ class ItemsViewController: UITableViewController{
         }
     }
     
+    override func viewDidLoad() {
+        tableView.rowHeight = UITableViewAutomaticDimension
+        tableView.estimatedRowHeight = 65
+    }
+    
+    
     // Returns number of items
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return itemStore.allItems.count
@@ -41,13 +47,14 @@ class ItemsViewController: UITableViewController{
     // Populates a cell view with data from an item
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         // create an instance of UTTableViewCell, with default appearance
-        let cell = tableView.dequeueReusableCell(withIdentifier: "UITableViewCell", for: indexPath)
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ItemCell", for: indexPath) as! ItemCell
         
         // set the text
         let item = itemStore.allItems[indexPath.row]
         
-        cell.textLabel?.text = item.name
-        cell.detailTextLabel?.text = "$\(item.valueInDollars)"
+        cell.nameLabel.text = item.name
+        cell.serialNumberLabel.text = item.serialNumber
+        cell.valueLabel.text = "$\(item.valueInDollars)"
         
         return cell
     }
